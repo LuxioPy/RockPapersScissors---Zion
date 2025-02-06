d8f45671a9c7a3d12d316c4acc0d11680f116af1
LuxioPy <zionceus@gmail.com>
2025-02-06 13:07:47 -0500

    Initial Commit

---
+++ RockPapersScissors/RockPapersScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPapersScissorsApp.swift
+//  RockPapersScissors
+//
+//  Created by Zion Ceus on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RockPapersScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
